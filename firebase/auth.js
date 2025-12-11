61ae86776411b3c51d2aa03591a295cc5090a2bc
EgeAbak2004 <egeabak67@gmail.com>
2025-12-12 02:48:21 +0300

    v8

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getDatabase, ref, get, child, push, set } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -31,8 +31,9 @@ const loginbtn = document.getElementById("loginbtn");
 const registerhref = document.getElementById("registerhref");
 const loginhref = document.getElementById("loginhref");
 const table = document.getElementById("table");
-
-
+const exitbtn = document.getElementById("exitbtn");
+const wring = document.getElementById("wring");
+const loadinmain = document.getElementById("loadinmain");
 const uploadmain = document.getElementById("upoadmain");
 
 if (registerbtn != null) {
@@ -44,18 +45,40 @@ if (loginbtn != null) {
 
 }
 
+if (exitbtn != null) {
+  exitbtn.addEventListener("click", () => Exit())
+
+  const Exit = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Kullanıcı çıkış yaptı");
+        window.location.href = "login.html";
+      })
+      .catch((error) => {
+        console.error("Çıkış hatası:", error);
+      });
+  }
+}
 
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
     registerhref.style.display = "none";
     loginhref.style.display = "none";
+    exitbtn.style.display = "flex";
     if (uploadmain != null) {
       uploadmain.style.display = "flex";
-    }
 
+    }
+    if (wring != null) {
+      wring.style.display = "none";
+    }
     if (table != null) {
       table.style.display = "flex";
+    }
+
+    if (loadinmain != null) {
+      loadinmain.style.display = "flex";
     }
 
   }
@@ -64,11 +87,18 @@ onAuthStateChanged(auth, (user) => {
     loginhref.style.display = "flex";
     if (uploadmain != null) {
       uploadmain.style.display = "none";
+      if (wring != null) {
+        wring.style.display = "flex";
+      }
 
+      if (loadinmain != null) {
+        loadinmain.style.display = "none";
+      }
     }
     if (table != null) {
       table.style.display = "none";
     }
+    exitbtn.style.display = "none";
   }
 })
 const Register = async () => {
@@ -134,44 +164,45 @@ const FilesDataGet = () => {
   if (table != null) {
 
 
-    const loadinmain = document.getElementById("loadinmain");
-    loadinmain.style.display = "flex";
-    table.style.display = "none";
+
+
     onAuthStateChanged(auth, (user) => {
 
+      if (user) {
+        loadinmain.style.display = "flex";
+        table.style.display = "none";
 
 
 
 
+        const dbref = ref(db);
+        get(child(dbref, `upload/${user.uid}`)).then((snap) => {
 
-      const dbref = ref(db);
-      get(child(dbref, `upload/${user.uid}`)).then((snap) => {
-
-        if (snap.exists()) {
-          const tablespawn = document.getElementById("filetable");
-
+          if (snap.exists()) {
+            const tablespawn = document.getElementById("filetable");
 
 
-          snap.forEach(data => {
-            let tr = document.createElement("tr");
-            let td = document.createElement("td");
-            let td2 = document.createElement("td");
-            let td3 = document.createElement("td");
-            let td4 = document.createElement("td");
-            let td5 = document.createElement("td");
-            td.textContent = data.child("type").val();
-            td2.textContent = data.child("name").val();
-            td3.textContent = data.child("size").val() + "by";
-            td4.textContent = data.child("date").val();
-            tr.append(td)
-            tr.append(td2)
-            tr.append(td3)
-            tr.append(td4)
-            tr.append(td5)
-            tablespawn.appendChild(tr);
+
+            snap.forEach(data => {
+              let tr = document.createElement("tr");
+              let td = document.createElement("td");
+              let td2 = document.createElement("td");
+              let td3 = document.createElement("td");
+              // let td4 = document.createElement("td");
+              let td5 = document.createElement("td");
+              td.textContent = data.child("name").val();
+              td2.textContent = data.child("type").val();
+              td3.textContent = data.child("size").val() + "by";
+              // td4.textContent = data.child("date").val();
+              tr.append(td)
+              tr.append(td2)
+              tr.append(td3)
+              // tr.append(td4)
+              tr.append(td5)
+              tablespawn.appendChild(tr);
 
 
-            td5.innerHTML = `
+              td5.innerHTML = `
   <a 
     href="https://res.cloudinary.com/dvdw9titb/image/upload/fl_attachment/v1765491358/${data.child("id").val()}.${data.child("type").val()}"
     download="${data.child("id").val()}.${data.child("type").val()}"
@@ -180,17 +211,22 @@ const FilesDataGet = () => {
     <i class="fa-solid fa-download"></i>
   </a>
 `;
-            loadinmain.style.display = "none";
-            table.style.display = "flex";
-          })
+              loadinmain.style.display = "none";
+              table.style.display = "flex";
+            })
 
-        }
-      })
-        .catch((err) => {
-          alert("Birşeyler ters gitti lütfen sonra deneyniz");
+          }
         })
-
+          .catch((err) => {
+            alert("Birşeyler ters gitti lütfen sonra deneyniz");
+          })
+      }
+      else {
+        loadinmain.style.display = "none";
+        table.style.display = "flex";
+      }
     })
+
   }
 }
 
